084a1be3bb0b53bc8abb3aed8334659b56f8bcfc
mrmitchboyer <mitch@mitchboyer.com>
2014-07-30 11:30:20 -0400

    builds first form

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,14 @@
 class Question < ActiveRecord::Base
   attr_accessor :text, :selection
+
+  def self.weight
+    {
+      "I don't care" => 0,
+      "I care a little" => 1,
+      "Whatever" => 2,
+      "I care a decent amount" => 3,
+      "I care so much it hurts" => 4
+    }
+  end
+
 end
